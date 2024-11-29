67cadbdced0700f2e18946ea9fb08f7305b30067
Luquian Cesar <luquian.santos@outlook.com>
2024-11-29 11:22:18 -0300

    chore: Diretério Microserviço Estoque criado

---
+++ msestoque/src/main/java/com/github/LukeCesar7/msestoque/MsestoqueApplication.java
@@ -0,0 +1,13 @@
+package com.github.LukeCesar7.msestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsestoqueApplication.class, args);
+	}
+
+}
